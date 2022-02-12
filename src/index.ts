47c5bdc686ac52c6fc08aac3ec6d1354953c5c01
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-02-12 13:43:34 -0500

    Fixed routes.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 // Depencencies and Initializers
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 const server: Application = express();
 import usersRouter from './routes/users';
-
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
 
 // Routes
 server.use('/api/users', usersRouter);
 
-server.use((req, res) => {
-	res.json({ message: 'OK!' });
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.get('/', (req: Request, res: Response) => {
+	res.status(200).json({ message: 'Welcome to the API' });
 });
 
 const PORT = process.env.PORT || 4000;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, Router } from 'express';
 const usersRouter: Router = express.Router();
 
-usersRouter.get('/api/users', (req: Request, res: Response) => {
+usersRouter.get('/', (req: Request, res: Response) => {
 	res.json({ message: 'User Router Connected' });
 });
 
